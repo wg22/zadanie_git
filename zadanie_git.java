9b1f1b8ca03d1a91e17ca1f630547e7104b37615
wg22 <wojciechwojciechgrudzien@gmail.com>
2021-08-02 01:04:40 +0200

    Update zadanie_git.java
    develop 3

--- zadanie_git.java
+++ zadanie_git.java
@@ -1,6 +1,6 @@
 public class zadanie_git {
     public static void main(String[] args) {
         System.out.println("zadanie git");
-		System.out.println("develop 2");
+		System.out.println("develop 3");
     }
 }
