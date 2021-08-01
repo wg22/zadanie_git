57f97fc4240bf574087b8def1697fda507fc10c4
wg22 <wojciechwojciechgrudzien@gmail.com>
2021-08-02 01:58:36 +0200

    Update zadanie_git.java

--- zadanie_git.java
+++ zadanie_git.java
@@ -4,7 +4,7 @@ public class zadanie_git {
 		System.out.println("develop 6");
 		 System.out.println("hotfix");
 		System.out.println("develop 3");
-		System.out.println("feature 2.2");
+		System.out.println("feature 2.3");
 		System.out.println("release 1.1");
 
     }
